8c0f37c82ef3e59b37d8f248864e5a7747fdc447
Albano Mazzino <albimazzino@gmail.com>
2023-05-03 14:44:03 -0300

    Nombre es ahora único en cada categoría

--- database/migrations/2023_04_01_133635_create_categorias_table.php
+++ database/migrations/2023_04_01_133635_create_categorias_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('categorias', function (Blueprint $table) {
             $table->id();
             $table->string('imagen_ruta')->nullable();
-            $table->string('nombre');
+            $table->string('nombre')->unique();
             $table->string('descripcion');
             $table->timestamps();
         });
